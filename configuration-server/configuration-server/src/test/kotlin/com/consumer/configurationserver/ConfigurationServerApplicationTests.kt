ad4ddb892bf7ab893d4c35a37da27c24cd3e7ab8
Petter55 <e.petter@hotmail.com>
2020-06-13 22:24:32 -0300

    config server added

---
+++ configuration-server/configuration-server/src/test/kotlin/com/consumer/configurationserver/ConfigurationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.consumer.configurationserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
